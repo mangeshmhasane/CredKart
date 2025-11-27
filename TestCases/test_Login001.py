a39a4285da1741236b5787977560e79cc7d4e787
mangeshmhasane <mangeshmh91@gmail.com>
2025-11-27 14:42:55 +0530

    Running Successfully

--- TestCases/test_Login001.py
+++ TestCases/test_Login001.py
@@ -1,29 +1,42 @@
 from Utilities import readconfig
 from PageObject.LoginPage import LoginClass
+from Utilities.readExcel import readExcel
 
 class Test_UserLogin001:
     def test_login_001(self, setup):
         self.driver = setup
         self.ul = LoginClass(self.driver)
 
-        # Open Url
-        self.driver.get(readconfig.getLoginUrl())
+        filename = "D:\\CAREER\\Pytest Practice\\CredKart 2025-26\\TestData\\Login Test Credentials.xlsx"
+        sheetname = "Sheet1"
+        rows = readExcel.get_row_count(filename=filename, sheetname=sheetname)
+        for r in range(2, rows+1):
+            userEmail = readExcel.read_data(filename=filename, sheetname=sheetname, row=r, column=1)
+            userPassword = readExcel.read_data(filename=filename, sheetname=sheetname, row=r, column=2)
+            expected_result = readExcel.read_data(filename=filename, sheetname=sheetname, row=r, column=3)
 
-        # Enter Email
-        self.ul.Enter_Email(readconfig.getLoginEmail())
+            # Open Url
+            self.driver.get(readconfig.getLoginUrl())
 
-        # Enter Password
-        self.ul.Enter_Password(readconfig.getLoginPassword())
+            # Enter Email
+            # self.ul.Enter_Email(readconfig.getLoginEmail())
+            self.ul.Enter_Email(userEmail)
 
-        # Click on login Button
-        self.ul.ClickonLoginButton()
+            # Enter Password
+            # self.ul.Enter_Password(readconfig.getLoginPassword())
+            self.ul.Enter_Password(userPassword)
 
-        # Validate login Page
-        try:
-            self.ul.ValidateLoginPage()
-            print("Login Successful...")
-            assert True
-        except:
-            print("Error: Login Unsuccessful...")
-            assert False
-        
+            # Click on login Button
+            self.ul.ClickonLoginButton()
+
+            # Validate login Page
+            if self.ul.ValidateLoginPage() == True and expected_result == "Pass":
+                print("Positive test passed...")
+                assert True
+            elif self.ul.ValidateLoginPage() == False and expected_result == "Fail":
+                print("Negative test passed...")
+                assert True
+            else:
+                print("Test fail (Mismatch between expected result and actual result)")
+                assert False
+            self.driver.delete_all_cookies()
---
+++ Utilities/readExcel.py
@@ -0,0 +1,24 @@
+import openpyxl
+
+class readExcel:
+    def get_row_count(filename, sheetname):
+        workbook = openpyxl.load_workbook(filename=filename)
+        sheet = workbook[sheetname]
+        return sheet.max_row
+
+    def get_column_count(filename, sheetname):
+        workbook = openpyxl.load_workbook(filename=filename)
+        sheet = workbook[sheetname]
+        return sheet.max_column
+
+    def read_data(filename, sheetname, row, column):
+        workbook = openpyxl.load_workbook(filename=filename)
+        sheet = workbook[sheetname]
+        return sheet.cell(row=row, column=column).value
+
+    def write_data(filename, sheetname, row, column, data):
+        workbook = openpyxl.load_workbook(filename=filename)
+        sheet = workbook[sheetname]
+        sheet.cell(row=row, column=column).value = data
+        workbook.save(filename=filename)
+    
