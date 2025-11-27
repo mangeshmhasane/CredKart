196ae782d7a0f64c7ea5fa35c1cc909d8375b4a9
mangeshmhasane <mangeshmh91@gmail.com>
2025-11-27 13:04:29 +0530

    test_Login001 file added

---
+++ TestCases/test_Login001.py
@@ -0,0 +1,29 @@
+from Utilities import readconfig
+from PageObject.LoginPage import LoginClass
+
+class Test_UserLogin001:
+    def test_login_001(self, setup):
+        self.driver = setup
+        self.ul = LoginClass(self.driver)
+
+        # Open Url
+        self.ul.get(readconfig.getLoginUrl())
+
+        # Enter Email
+        self.ul.Enter_Email(readconfig.getLoginEmail())
+
+        # Enter Password
+        self.ul.Enter_Password(readconfig.getLoginPassword())
+
+        # Click on login Button
+        self.ul.ClickonLoginButton()
+
+        # Validate login Page
+        try:
+            self.ul.ValidateLoginPage()
+            print("Login Successful...")
+            assert True
+        except:
+            print("Error: Login Unsuccessful...")
+            assert False
+        
