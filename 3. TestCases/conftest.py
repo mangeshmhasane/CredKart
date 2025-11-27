6cdc056bcb4a8d3bbe1d93392061ad5a9dfe2794
mangeshmhasane <mangeshmh91@gmail.com>
2025-11-27 12:36:48 +0530

    conftest file created

---
+++ 3. TestCases/conftest.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import pytest
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store",default="firefox",help="Type of browser: chrome, firefox, edge")
+
+@pytest.fixture
+def browser(request):
+    return request.config.getoption("--browser")
+
+@pytest.fixture
+def setup(browser):
+    if browser == 'chrome':
+        print("Launching Chrome Browser.")
+        driver = webdriver.Chrome()
+    
+    elif browser == 'firefox':
+        print("Launching Firefox Browser.")
+        driver = webdriver.Firefox()
+    
+    elif browser == 'edge':
+        print("Launching Firefox Browser.")
+        driver = webdriver.Edge()
+    
+    else:
+        print("Invalid Browser, Launching Firefox browser.")
+        driver = webdriver.Firefox()
+
+    yield browser
+    driver.quit()
